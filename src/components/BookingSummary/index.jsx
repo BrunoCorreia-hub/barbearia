1825bf951bf1f0ceabcc14e33cf3210b81f161d7
BrunoCorreia-hub <correiabruno346@gmail.com>
2026-06-05 22:27:41 -0300

    continuacao da estillizacao de tela de confirmacao de agendamento

--- src/components/BookingSummary/index.jsx
+++ src/components/BookingSummary/index.jsx
@@ -1,47 +1,51 @@
 import { Formater } from "../../utils/Formater";
-import { FormBox, Summary, SummaryRow, FieldGroup, Field, Label, Input } from "./styles";
+import { FormBox, Summary, Title, SummaryGrid, SummaryRow, FieldGroup, Field, Label, Input } from "./styles";
 
-function BookingSummary({ selectedService, setSelectedService, selectedDay, setSelectedDay, calMonth, selectedTime, calYear, name, setName, phone, setPhone }){
-    return(
+function BookingSummary({ selectedService, /* setSelectedService, */ selectedDay,/*  setSelectedDay, */ calMonth, selectedTime, calYear, name, setName, phone, setPhone }) {
+    return (
         <FormBox>
             <Summary>
+                <Title>RESUMO DO SERVIÇO</Title>
+                <SummaryGrid>
+                    <SummaryRow>
+                        <Label> ✂ Serviço:</Label>
+                        <h2>{selectedService?.name}</h2>
+                    </SummaryRow>
+                    <SummaryRow>
+                        <Label> 🗓 Data:</Label>
+                        <span>{selectedDay}/{calMonth + 1}/{calYear}</span>
+                    </SummaryRow>
+                    <SummaryRow>
+                        <Label> ⏱ Horário:</Label>
+                        <span>{selectedTime}</span>
+                    </SummaryRow>
+                    <SummaryRow>
+                        <Label> ⏳ Duração:</Label>
+                        <span>{selectedService?.time}</span>
+                    </SummaryRow>
+                </SummaryGrid>
                 <SummaryRow>
-                    <Label>Serviço</Label>
-                    <span>{selectedService?.name}</span>
-                </SummaryRow>
-                <SummaryRow>
-                    <Label>Data</Label>
-                    <span>{selectedDay}/{calMonth + 1}/{calYear}</span>
-                </SummaryRow>
-                <SummaryRow>
-                    <Label>Horário</Label>
-                    <span>{selectedTime}</span>
-                </SummaryRow>
-                <SummaryRow>
-                    <Label>Duração</Label>
-                    <span>{selectedService?.time}</span>
-                </SummaryRow>
-                <SummaryRow>
-                    <Label>Total</Label>
-                    <span>{Formater(selectedService?.price)}</span>
+                    <Label>Total:</Label>
+                    <h2>{Formater(selectedService?.price)}</h2>
                 </SummaryRow>
             </Summary>
 
             <FieldGroup>
+                <Title>SEUS DADOS</Title>
                 <Field>
-                    <Label>Nome Completo</Label>
-                    <Input 
-                     placeholder="Seu nome"
-                     value={name}
-                     onChange={(e) => setName(e.target.value)}
+                    <Label>Nome Completo:</Label>
+                    <Input
+                        placeholder="Seu nome"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </Field>
                 <Field>
-                    <Label>Telefone / WhatsApp</Label>
-                    <Input 
-                     placeholder="(00) 00000-0000"
-                     value={phone}
-                     onChange={(e) => setPhone(e.target.value)}
+                    <Label>Telefone / WhatsApp:</Label>
+                    <Input
+                        placeholder="(00) 00000-0000"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
                     />
                 </Field>
             </FieldGroup>
--- src/components/BookingSummary/styles.js
+++ src/components/BookingSummary/styles.js
@@ -1,24 +1,78 @@
 import styled from "styled-components";
 
-export const FormBox = styled.form``;
+export const FormBox = styled.form`
+    padding: 3rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 3rem;
+`;
 
-export const Summary = styled.div``;
+export const Summary = styled.div`
+    background-color: ${props => props.theme.backgroundSecond};
+    padding: 2rem;
+    border-radius: 1rem;
+    border: 0.1rem solid ${props => props.theme.textSecond};
+`;
+
+export const Title = styled.h1`
+    color: ${props => props.theme.textSecond};
+    margin-bottom: 0.8rem;
+    font-weight: 400;  
+`;
+
+export const SummaryGrid = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem 0;
+    border-top: 0.1rem solid ${props => props.theme.textSecond};
+    border-bottom: 0.1rem solid ${props => props.theme.textSecond};
+    margin-bottom: 2rem;
+`;
 
 export const SummaryRow = styled.div`
     display: flex;
-    gap: 3rem;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5rem;
 
     span{
         color: ${props => props.theme.white};
+        font-size: 2rem;
+    }
+    h2{
+        color: ${props => props.theme.bronze};
+        font-size: 2rem;
     }
 `;
 
-export const FieldGroup = styled.div``;
-
-export const Field = styled.div``;
-
-export const Label = styled.label`
-    color: ${props => props.theme.white};
+export const FieldGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: ${props => props.theme.backgroundSecond};
+    padding: 2rem;
+    border-radius: 1rem;
 `;
 
-export const Input = styled.input``;
+export const Field = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`;
+
+export const Label = styled.label`
+    color: ${props => props.theme.textSecond};
+    font-size: 2rem;
+`;
+
+export const Input = styled.input`
+    padding: 0.5rem;
+    background-color: transparent;
+    border-radius: 0.5rem;
+    border: 0.1rem solid ${props => props.theme.secondBlack};
+    font-size: 2rem;
+    font-weight: 600;
+    color: ${props => props.theme.secondBlack};
+`;
